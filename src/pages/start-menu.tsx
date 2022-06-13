2dd890a5d2b7999acd3e6745c801da9d8c2c02b1
“avganka” <karambasterg@gmail.com>
2022-06-13 23:38:36 +0300

    Добавлен логотив в svg

--- src/pages/start-menu.tsx
+++ src/pages/start-menu.tsx
@@ -6,11 +6,12 @@ function StartMenu(): JSX.Element {
   return (
     <div className='app'>
       <div className='logo'>
-        <img src='./logo.png' alt='logo' />
+        <img src='./logo.svg' alt='logo' width='300px'/>
       </div>
       <div className='menu'>
         <Link to={AppRoutes.TEAMS} className='button button-menu'>Начать игру</Link>
         <Link to={AppRoutes.GAME} className='button button-menu'>Раунд</Link>
+        <Link to={AppRoutes.QUESTION} className='button button-menu'>Вопрос</Link>
       </div>
     </div>
   );
