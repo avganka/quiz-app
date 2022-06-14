c9bc90016c496ea2c7df34c311e0176a6d183c41
avganka <karambasterg@gmail.com>
2022-06-14 16:35:45 +0300

    Исправлен Player

--- src/components/player/player.tsx
+++ src/components/player/player.tsx
@@ -1,18 +1,14 @@
 import { SyntheticEvent, useEffect, useRef, useState } from 'react';
-// import ProgressBar from '../progress-bar/progress-bar';
-// import ProgressBar from '../progress-bar/progress-bar';
-// import styles from './audio-player.module.scss';
-
 
 const START_POS = 20;
-const SONG_FRAGMENT_DURATION = 6;
 
 type PlayerProps = {
   src: string,
+  duration: number,
 }
 
-function Player({ src }: PlayerProps): JSX.Element {
-  const [songProgress, setSongProgress] = useState(0);
+function Player({ src, duration }: PlayerProps): JSX.Element {
+  const songDuration = duration / 3;
   const [isMusicPlaying, setIsMusicPlaying] = useState(false);
   const track = useRef<HTMLAudioElement>(null);
 
@@ -44,38 +40,28 @@ function Player({ src }: PlayerProps): JSX.Element {
   const onTimeUpdateHandler = (evt: SyntheticEvent<HTMLAudioElement>) => {
     const song = evt.currentTarget;
 
-    if (song.currentTime >= (START_POS + SONG_FRAGMENT_DURATION)) {
+    if (song.currentTime >= (START_POS + songDuration)) {
       song.pause();
       setIsMusicPlaying(false);
     }
 
-    if ((song.currentTime - START_POS) > (SONG_FRAGMENT_DURATION - 1)) {
+    if ((song.currentTime - START_POS) > (songDuration - 1)) {
       song.volume = 0.7;
     }
-    if ((song.currentTime - START_POS) > (SONG_FRAGMENT_DURATION)) {
+    if ((song.currentTime - START_POS) > (songDuration)) {
       song.volume = 0.3;
     }
-
-    setSongProgress((song.currentTime - START_POS) * 100 / SONG_FRAGMENT_DURATION);
   };
 
 
   return (
-    // <div className={styles.container}>
-    //   <audio ref={track} preload="metadata" onTimeUpdate={onTimeUpdateHandler} >
-    //     <source src={src} type="audio/mp3" />
-    //     Ваш web-браузер не поддерживает HTML элемент «<a href="/html/audio/">audio</a>».
-    //   </audio>
-    //   <div onClick={controlMusicHandler} className={`${styles['control-button']} ${isMusicPlaying ? styles['control-button--paused'] : ''}`}></div>
-    //   <ProgressBar progressPercent={songProgress} />
-    // </div>
     <div className="wrapper">
       <div className={`player-controls ${isMusicPlaying ? 'playing' : ''}`} onClick={controlMusicHandler}>
         <span className="play control-button" ></span>
         <span className="pause control-button--paused"></span>
       </div>
       <audio id="player" ref={track} onTimeUpdate={onTimeUpdateHandler}>
-        <source src="https://www.dropbox.com/s/t32waag3ib20b28/OneDance-Drake.mp3?raw=1" type="audio/mp3" />
+        <source src={src} type="audio/mp3" />
       </audio>
     </div>
 
